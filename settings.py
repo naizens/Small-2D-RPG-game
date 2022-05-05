a770fbb9513b99b9535571cb32fbf23e1956891e
Niclas Heide <niclas.heide@icloud.com>
2022-05-05 18:53:18 +0200

    inital commit

---
+++ settings.py
@@ -0,0 +1,18 @@
+import os
+
+class Settings():
+    width = 1280
+    heigth = 720
+    fps = 60
+
+    title = "Tilemap Editor"
+    volume = 0.1
+
+    file_path = os.path.dirname(os.path.abspath(__file__))
+    asset_path = os.path.join(file_path, "assets")
+    image_path = os.path.join(asset_path, "images")
+    sound_path = os.path.join(asset_path, "sounds")
+
+    tilesize = 16 # 16x16 tiles
+    
+
