623da318343450c058410e146b479b5d27b259e6
Niclas Heide <niclas.heide@icloud.com>
2022-05-05 19:28:13 +0200

    added window title and changed loop a bit

--- main.py
+++ main.py
@@ -2,23 +2,35 @@ import pygame, sys
 from settings import *
 from debug import *
 
-class Game():
-    def __init__(self):
+class Game(object):
+    def __init__(self) -> None:
+        super().__init__()
+        os.environ["SDL_VIDEO_WINDOW_CENTERED"] = "10,50"
+
 
         pygame.init()
+        pygame.display.set_caption(Settings.title)
+
         self.screen = pygame.display.set_mode((Settings.width, Settings.heigth))
         self.clock = pygame.time.Clock()
 
     def run(self):
-        while True:
+        self.running = True
+
+        self.screen.fill("black")
+        self.clock.tick(Settings.fps)
+
+        while self.running == True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
+                    self.running = False
 
-            self.screen.fill("black")
             pygame.display.update()
-            self.clock.tick(Settings.fps)
+            
+
+        pygame.quit()
+        sys.exit()
+   
 
 if __name__ == "__main__":
     game = Game()
