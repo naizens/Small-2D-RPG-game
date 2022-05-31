c8139504939665cbeae9727c843f23ba4eb36198
Niclas Heide <niclas.heide@icloud.com>
2022-05-31 16:17:16 +0200

    added weapons paths and images

--- settings.py
+++ settings.py
@@ -16,6 +16,14 @@ class Settings():
     animal_path = os.path.join(image_path, "animals")
     monster_path = os.path.join(image_path, "monsters")
     character_path = os.path.join(image_path, "player")
+    weapon_path = os.path.join(image_path, "weapons")
+    
+    #pathes for the weapons
+    sword_path = os.path.join(weapon_path, "sword")
+    lance_path = os.path.join(weapon_path, "lance")
+    axe_path = os.path.join(weapon_path, "axe")
+    rapier_path = os.path.join(weapon_path, "rapier")
+    sai_path = os.path.join(weapon_path, "sai")
     
     map_path = os.path.join(asset_path, "map")
     sound_path = os.path.join(asset_path, "sounds")
@@ -25,3 +33,10 @@ class Settings():
     tilesize = 16 # set the tilesize to 16x16
     scaling = 4 # parameter to scale the images
     
+    weapon_data = {
+        "sword": {"cooldown": 100, "damage": 15,"graphic": os.path.join(sword_path, "sword.png")},
+        "lance": {"cooldown": 400, "damage": 30,"graphic": os.path.join(lance_path, "lance.png")},
+        "axe": {"cooldown": 300, "damage": 20,"graphic": os.path.join(axe_path, "axe.png")},
+        "rapier": {"cooldown": 50, "damage": 8,"graphic": os.path.join(rapier_path, "rapier.png")},
+        "sai": {"cooldown": 80, "damage": 10,"graphic": os.path.join(sai_path, "sai.png")}
+    }
