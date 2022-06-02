e5f042b6ce96237019046ba45dca988b17cb7531
Niclas Heide <niclas.heide@icloud.com>
2022-06-02 17:51:52 +0200

    added weapon direction

--- weapons.py
+++ weapons.py
@@ -1,12 +1,23 @@
+from re import S
 import pygame
 
 class Weapon(pygame.sprite.Sprite):
     def __init__(self, player, groups) -> None:
         super().__init__(groups)
-        directon = player.staus
+        directon = player.status.split("_", 1)[0]
+        print(directon)
         
         #graphics
         self.image = pygame.Surface((40, 40))
         
         #placement
-        self.rect = self.image.get_rect(center = player.rect.center)
+        if directon == "right":
+            self.rect = self.image.get_rect(midleft = player.rect.midright + pygame.math.Vector2(-4, 16))
+        elif directon == "left":
+            self.rect = self.image.get_rect(midright = player.rect.midleft + pygame.math.Vector2(4, 16))
+        elif directon == "up":
+            self.rect = self.image.get_rect(midbottom = player.rect.midtop + pygame.math.Vector2(-16, 4))
+        elif directon == "down":
+            self.rect = self.image.get_rect(midtop = player.rect.midbottom + pygame.math.Vector2(-16, 0))
+        else:
+            self.rect = self.image.get_rect(center = player.rect.center)
